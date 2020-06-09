36010df7880da13065d361a28b40c3c4c10c43e9
Samuglz6 <samuel.gonzalez3@alu.uclm.es>
2020-06-09 12:20:44 +0200

    servicio de propagacion implementado (no completo)

--- src/intermediate.py
+++ src/intermediate.py
@@ -4,6 +4,7 @@
 import os
 import sys
 import Ice
+import IceStorm
 Ice.loadSlice('./src/trawlnet.ice')
 import TrawlNet
 
@@ -17,13 +18,41 @@ class IntermediateI(TrawlNet.Intermediate):
         self.server.execute(message)
 
 class Intermediate(Ice.Application):
+    def get_topic_manager(self):
+        key = 'IceStorm.TopicManager.Proxy'
+        proxy = self.communicator().propertyToProxy(key)
+        if proxy is None:
+            print("ERROR: Property", key, "not set")
+            return None
+        
+        print("Using IceStorm in: '%s'" % key)
+        return IceStorm.TopicManagerPrx.checkedCast(proxy)
+
     def run(self, argv):
+        t_manager =  self.get_topic_manager()
+        if not t_manager:
+            print("ERROR: Invalid proxy.")
+            return 2
+        
         broker = self.communicator()
         servant = IntermediateI()
 
         adapter = broker.createObjectAdapter("IntermediateAdapter")
-        proxy = adapter.add(servant, broker.stringToIdentity("intermediate1"))
+        subscriber = adapter.addWithUUID(servant)
 
+        t_name = "IntermediateTopic"
+        qos = {}
+        try:
+            topic = t_manager.retrieve(t_name)
+        except IceStorm.NoSuchTopic:
+            topic = t_manager.create(t_name)
+
+        topic.subscribeAndGetPublisher(qos, subscriber)
+        print("Waiting for events...", subscriber)
+
+
+
+        '''
         print(proxy, flush=True)
 
         proxyServer = self.communicator().stringToProxy(argv[1])
@@ -33,14 +62,19 @@ class Intermediate(Ice.Application):
             servant.server = server
         else:
             raise RuntimeError('ERROR: The given proxy is not valid.')
-
+        '''
         adapter.activate()
         self.shutdownOnInterrupt()
         broker.waitForShutdown()
 
+        topic.unsubscribe(subscriber)
+
         return 0
 
+sys.exit(Intermediate().main(sys.argv))
+'''
 if __name__ == '__main__':
     app = Intermediate()
     exit_status = app.main(sys.argv)
     sys.exit(exit_status)
+'''    
